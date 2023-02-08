a67b437c58d8bcb672b538c47b9466ef5e7a3cc5
GreatGamer <121580579+ggtech360@users.noreply.github.com>
2023-02-08 15:40:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function Clicked(){
+    const menu = document.querySelector('.container');
+    const page = document.querySelector('.item');
+    menu.style.display = 'none';
+    page.style.display = 'block';
+}
